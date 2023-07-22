fe9ab891156df302a64cdb40d057921309f84134
mdferdousalam <mdferdousalam1989@yahoo.com>
2023-07-22 16:01:56 +0600

    routes setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,41 @@
+import {createBrowserRouter} from "react-router-dom"
+import Home from "../page/Home";
+import App from "../App";
+import Login from "../page/Login";
+import Registration from "../page/Registration";
+import AddBook from "../page/AddBook";
+import BookDetails from "../page/BookDetails";
+import Profile from "../page/Profile";
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element:<App/>
+    },
+    {
+        path: "/home",
+        element:<Home></Home>
+    },
+    {
+        path: "/login",
+        element:<Login></Login>
+    },
+    {
+        path: "/registration",
+        element:<Registration></Registration>
+    },
+    {
+        path: "/addbook",
+        element:<AddBook></AddBook>
+    },
+    {
+        path: "/bookdetails",
+        element:<BookDetails></BookDetails>
+    },
+    {
+        path: "/profile",
+        element:<Profile></Profile>
+    },
+
+]);
+
+export default routes;
